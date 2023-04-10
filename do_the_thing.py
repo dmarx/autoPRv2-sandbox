81b0de6ea92870f8732abd7fed30f2d1b6885913
David Marx <david.marx84@gmail.com>
2023-04-09 18:01:41 -0700

    Update do_the_thing.py

--- do_the_thing.py
+++ do_the_thing.py
@@ -1,7 +1,19 @@
 import sys
+import json
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    print(len(args))
-    for i, a in enumerate(args):
-        print(a)
+    #args = sys.argv[1:]
+    issue_json = json.loads(sys.argv[1])
+    #print(issue_json)
+    
+    title = issue_json['title']
+    body = issue_json['body']
+    author_role = issue_json['author_association'] #"author_association": "OWNER",
+    
+    if author_role != 'OWNER':
+        exit()
+    
+    prompt = f"issue title: {title}\nissue description: {body}"
+    print("<prompt>")
+    print(prompt)
+    print("</prompt>)
