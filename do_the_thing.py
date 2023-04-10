02eafa278bb3d6ec0b18392d2fe65532e5fe235e
David Marx <david.marx84@gmail.com>
2023-04-09 20:42:48 -0700

    Update do_the_thing.py

--- do_the_thing.py
+++ do_the_thing.py
@@ -6,13 +6,26 @@ def prompt_from_issue(issue: dict):
     """
     Given a github issue object, returns a simplified prompt.
     """
-    return f"issue title: {issue['title']}\nissue description: {issue['body']}"
+    prompt = f"issue title: {issue['title']}\nissue description: {issue['body']}"
+    logger.info(f"<prompt>{prompt}</prompt>")
+    return prompt
     
+class Agent:
+    def __init__(self, issue):
+        self.issue = issue
+        self.directive = prompt_from_issue(issue)
+    def address_issue(self):
+        self.summarize_directive()
+        self.make_a_plan()
+        self.step_through_plan()
+        self.validate_solution()
+        self.submit_pr()
+
 
 if __name__ == '__main__':
     issue = json.loads(sys.argv[1])
     author_role = issue['author_association']
     if author_role != 'OWNER':
         exit()
-    prompt = prompt_from_issue(issue)
-    logger.info(f"<prompt>{prompt}</prompt>")
+    agent = Agent(issue)
+    agent.address_issue()
